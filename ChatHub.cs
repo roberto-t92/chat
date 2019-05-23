e69de5a6b8de0a521d8ae8c8147534c36b9490a8
Roberto <rm.torres010392@gmail.com>
2019-05-23 12:52:00 -0500

    init commit

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string User, string Msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", User, Msg);
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿//crear conexion
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+//recibir el mensaje
+connection.on("ReceiveMessage", (user, message) => {
+    const objDate = new Date(),
+        hr = objDate.getHours(),
+        min = (objDate.getMinutes() < 10 ? '0' : '') + objDate.getMinutes(),
+        seg = (objDate.getSeconds() < 10 ? '0' : '') + objDate.getSeconds();
+    let hora = hr + ":" + min + ":" + seg;    
+
+    const receiveHora = hora;
+    const receiveMessage = user + ": " + message;        
+    const li = document.createElement("li");    
+
+    li.textContent = receiveHora + " " + receiveMessage;
+    document.getElementById("messageList").appendChild(li);
+})
+
+connection.start().catch(err => console.error(err.toString()));
+
+//enviar mensaje
+document.getElementById("sendMessage").addEventListener("click", event => {
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+
+    connection.invoke("sendMessage", user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});
+
